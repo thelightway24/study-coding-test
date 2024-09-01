4c752199cacc200c53273f2058971fcdbdba69d0
The Light Way <147745679+YJ-circle@users.noreply.github.com>
2024-09-02 07:47:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.20 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,16 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        
-        if(s.length() != 4 && s.length() != 6){
-            answer = false;
-        }
-        
-        try {
-            Integer.parseInt(s);
-        } catch (Exception e){
-            answer = false;
-        }
-        return answer;
+        return s.matches("[0-9]{4}|[0-9]{6}");
     }
 }
