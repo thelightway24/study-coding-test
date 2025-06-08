e59ff14b682711569456dc42594e36db89e19f7b
Yejeong, Ham <thelightway@daum.net>
2025-06-09 01:30:37 +0900

    [Bronze V] Title: 윤년, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int y = Integer.parseInt(br.readLine());
+    if(y%4 == 0 && y%100 != 0){
+        System.out.println("1");
+    } else if (y%400 == 0){
+        System.out.println("1");
+    } else {
+        System.out.println("0");
+    }
+  }
+}
