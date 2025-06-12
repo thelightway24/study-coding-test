f0a05720af690c64edc462fb676364259e567393
Yejeong, Ham <thelightway@daum.net>
2025-06-12 21:50:47 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 664 ms, Memory: 241440 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
