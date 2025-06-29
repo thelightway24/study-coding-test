c9905d57ed84ac4a6e9413c12cbc896b0c5b998a
Yejeong, Ham <thelightway@daum.net>
2025-06-29 17:21:24 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,15 +1,13 @@
-SELECT
-    USER_ID, PRODUCT_ID
-FROM (
-    SELECT
-        USER_ID, PRODUCT_ID, count(*) as count
-    FROM
-        ONLINE_SALE
-    GROUP BY
-        USER_ID, PRODUCT_ID 
-    )
-WHERE
-    count > 1
-ORDER BY
+SELECT 
+    USER_ID,
+    PRODUCT_ID
+FROM
+    ONLINE_SALE
+GROUP BY
+    USER_ID, 
+    PRODUCT_ID
+HAVING
+    COUNT(*)>=2
+ORDER BY 
     USER_ID asc,
-    PRODUCT_ID desc
+    PRODUCT_ID desc;
