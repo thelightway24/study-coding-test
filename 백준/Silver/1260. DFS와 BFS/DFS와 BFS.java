3c6f7149a22a36398143b586fc3cbbb353f40da8
Yejeong, Ham <thelightway@daum.net>
2025-07-12 09:24:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 21868 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
