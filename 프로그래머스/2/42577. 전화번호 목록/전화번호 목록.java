1e020e3b1cb23f86f301296fa68e86da7c704e5d
Yejeong, Ham <thelightway@daum.net>
2025-07-20 22:41:12 +0900

    [level 2] Title: 전화번호 목록

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class Solution {
+    //해시 활용
     public boolean solution(String[] phone_book) {
         Map<Integer, Set<String>> map = new HashMap<>();
         for (String s : phone_book) {
@@ -12,6 +13,19 @@ class Solution {
                 if (set != null && set.contains(s.substring(0, i))) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+}
+
+class Solution2 {
+    public boolean solution(String[] phone_book) {
+        //인접값 비교 방법
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
         return true;
