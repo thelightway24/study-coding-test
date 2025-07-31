bb6091ca025255c450417a12d58f5e74c581e78e
Yejeong, Ham <thelightway@daum.net>
2025-08-01 01:45:10 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main (String[] args){
+        String s = """
+         ,r\'\"7
+r`-_   ,\'  ,/
+ \\. \". L_r\'
+   `~\\/
+      |
+      |
+            """;
+        System.out.println(s);
+    }
+}
